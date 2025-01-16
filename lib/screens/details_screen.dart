076b61e49eb22d5140fc0bc76b458e4e7f80d452
Mariusz.gluza45 <Mariusz.gluza45@gmail.com>
2025-01-17 00:57:20 +0100

    feat: movie details

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -13,17 +13,39 @@ class DetailsScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              movie['title'],
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
-            const SizedBox(height: 8.0),
-            Text(movie['overview']),
-            // Add more movie details here if needed
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Image.network(
+                  'https://image.tmdb.org/t/p/w500${movie['poster_path']}',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              Text(
+                movie['title'],
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+              const SizedBox(height: 8.0),
+              Text(
+                'Release Date: ${movie['release_date']}',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              const SizedBox(height: 8.0),
+              Text(
+                'Rating: ${movie['vote_average']}',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              const SizedBox(height: 16.0),
+              Text(
+                movie['overview'],
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              // Add more movie details here if needed
+            ],
+          ),
         ),
       ),
     );
